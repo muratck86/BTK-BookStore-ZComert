3cf7ff47af5c7129295bcad0185b1773b1de2ee7
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-27 05:27:12 +0300

    Content negotiation proper serialization for xml requests.

--- Entities/DataTransferObjects/BookDto.cs
+++ Entities/DataTransferObjects/BookDto.cs
@@ -1,5 +1,9 @@
 ﻿namespace Entities.DataTransferObjects
 {
-    [Serializable]
-    public record BookDto (int Id,  string Title, decimal Price) { }
+    public record BookDto () 
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public decimal Price { get; init; }
+    }
 }
