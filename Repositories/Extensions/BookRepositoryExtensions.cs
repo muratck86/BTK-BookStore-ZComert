097fadd9865ff31096f338fb93633fc4e6537edc
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-29 02:40:47 +0300

    Searching completed

--- Repositories/Extensions/BookRepositoryExtensions.cs
+++ Repositories/Extensions/BookRepositoryExtensions.cs
@@ -6,13 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories.EfCore
+namespace Repositories.Extensions
 {
     public static class BookRepositoryExtensions
     {
         public static IQueryable<Book> FilterByPrice(this IQueryable<Book> books, uint minPrice, uint maxPrice)
         {
             return books.Where(book => book.Price <= maxPrice && book.Price >= minPrice);
+        }
+
+        public static IQueryable<Book> Search(this IQueryable<Book> books, string searchText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+                return books;
+
+            var lowerCase = searchText.ToLower().Trim();
+            return books.Where(book => book.Title.ToLower().Contains(searchText));
         }
     }
 }
